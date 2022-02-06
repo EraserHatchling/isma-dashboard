8697f44843506e3b73cd2280766a10aa8bf0fe05
EraserHatchling <ismadaniel23sept@gmail.com>
2022-02-06 03:31:57 +0000

    commit2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,3 @@
-
 <script>
     import supabase from '$lib/db';
 
@@ -7,15 +6,50 @@
 
    	 if (error) alert(error.message); // alert if error
     }
-</script>
 
+    // Select entries
+    async function getEntries() {
+   	 const { data, error } = await supabase.from('moodEntries').select();
+   	 if (error) alert(error.message);
+
+   	 return data;
+    }
+</script>
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
 
 
 <!-- Sign Out -->
 <section class="container px-4 py-3 text-center">
     <button class="btn btn-secondary" on:click={signOut}>Logout</button>
 </section>
-
 
 
 
